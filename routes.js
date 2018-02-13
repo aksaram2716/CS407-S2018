6e4a37727a4165d9b5334ab1e3da77a5dcc6c79f
Travis Kovacic <tdkovaci@purdue.edu>
2018-02-13 17:01:55 -0500

    Added functionality to the signup and login buttons

--- routes.js
+++ routes.js
@@ -1,19 +1,25 @@
 var express = require('express')
 var router = express.Router()
 
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now())
-});
-
+//Handles typed requests
 router.get('/', function(req, res) {
     res.sendFile('./public/index.html', { root: __dirname });
 });
 
-router.get('/login', function(req, res) {
+router.get('/login*', function(req, res) {
     res.sendFile('./public/login.html', { root: __dirname });
 });
 
-router.get('/signup', function(req, res) {
+router.get('/signup*', function(req, res) {
+    res.sendFile('./public/signup.html', { root: __dirname });
+});
+
+//Handles button events
+router.get('loginPage', function(req, res) {
+    res.sendFile('./public/login.html', { root: __dirname });
+});
+
+router.get('signupPage', function(req, res) {
     res.sendFile('./public/signup.html', { root: __dirname });
 });
 
--- server.js
+++ server.js
@@ -10,16 +10,5 @@ var port = 8080;
 app.listen(port)
 console.log("Application started and listening on port: " + port);
 
-//Fetch index.html on login
-app.get('/', function(req, res) {
-    res.sendFile('./public/index.html', { root: __dirname });
-});
-
-app.get('/login', function(req, res) {
-    res.sendFile('./public/login.html', { root: __dirname });
-});
-
-app.get('/signup', function(req, res) {
-    res.sendFile('./public/signup.html', { root: __dirname });
-});
-//app.use('/index', router)
+//Attach router to app
+app.use(router)
