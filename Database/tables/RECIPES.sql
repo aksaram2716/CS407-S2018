d72126d531a397a57e2d960a6c9101b4b13003f2
Hari Mantena <Hari@Prithvis-MacBook-Pro.local>
2018-02-18 11:30:50 -0800

    Add schemas for table RECIPES, USER_INFO, USER_ACCINFO, USER_ACCINFOTYPE

---
+++ Database/tables/RECIPES.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localdb
+ Source Server Type    : MySQL
+ Source Server Version : 50718
+ Source Host           : localhost
+ Source Database       : JustRecipes
+
+ Target Server Type    : MySQL
+ Target Server Version : 50718
+ File Encoding         : utf-8
+
+ Date: 02/18/2018 14:45:18 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `RECIPES`
+-- ----------------------------
+DROP TABLE IF EXISTS `RECIPES`;
+CREATE TABLE `RECIPES` (
+  `RECIPE_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) NOT NULL,
+  `IMAGE` text NOT NULL,
+  `TEXT` text NOT NULL,
+  `OWNER_ID` int(11) NOT NULL,
+  `CREATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `LAST_MODIFIED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`RECIPE_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
