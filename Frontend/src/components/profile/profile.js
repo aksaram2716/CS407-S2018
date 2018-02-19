b3ba995202e0c0d81b37706d055bbf9882280dbd
Travis Kovacic <tdkovaci@purdue.edu>
2018-02-19 15:57:39 -0500

    Basic Profile page and start of templates

--- Frontend/src/components/profile/profile.js
+++ Frontend/src/components/profile/profile.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { TestComp } from './TestComp.js';
-
-//ReactDOM.render(<h1>TEST</h1>, document.getElementById('root'));
-
-//ReactDOM.render(<TestComp />, document.getElementById('root'));
+var app = angular.module("profile", []);
--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -6,6 +6,10 @@ var app = express();
 var router = require('./routes')
 var port = 8080;
 
+// Lets us link things in HTML using script and images links if the src
+// starts with "/components"
+app.use('/components',express.static(__dirname + '/components/'));
+
 // Start app!
 app.listen(port)
 console.log("Application started and listening on port: " + port);
