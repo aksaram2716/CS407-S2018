0dbc8068d1141df56a1b798d748eecbd5696cc0a
Travis Kovacic <tdkovaci@purdue.edu>
2018-02-19 13:22:41 -0500

    Test importing React Modules to profile

---
+++ Frontend/src/components/profile/TestComp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TestComp extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        My Application!
+      </div>
+    );
+  }
+}
+
+export default TestComp;
---
+++ Frontend/src/components/profile/profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { TestComp } from './TestComp.js';
+
+//ReactDOM.render(<h1>TEST</h1>, document.getElementById('root'));
+
+//ReactDOM.render(<TestComp />, document.getElementById('root'));
