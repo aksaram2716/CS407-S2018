e67a24655be1152fe4057401b7c54e57647c15d0
Travis Kovacic <tdkovaci@purdue.edu>
2018-02-19 21:11:56 -0500

    Added very basic infinite scrolling to the home page

---
+++ Frontend/src/components/home/recipeControl.js
@@ -0,0 +1,11 @@
+app.controller("recipeControl", function($scope) {
+    var length = 10;
+    $scope.images = [1,2,3,4,5,6,7,8,9,10];
+
+    $scope.loadMore = function(){
+        var last = $scope.images[$scope.images.length - 1];
+        for(var i = 1; i <=length; i++){
+            $scope.images.push(last + i);
+        }
+    };
+});
--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -2,17 +2,19 @@
 
 // Set up
 var express = require('express');
+var router = require('./routes');
+var path = require('path');
 var app = express();
-var router = require('./routes')
 var port = 8080;
 
 // Lets us link things in HTML using script and images links if the src
 // starts with "/components"
 app.use('/components',express.static(__dirname + '/components/'));
+// Needed to access the infinite scrolling script and/or other node_modules
+app.use('/node_modules',express.static(path.join(__dirname + '/../node_modules/')));
 
 // Start app!
 app.listen(port)
 console.log("Application started and listening on port: " + port);
-
 //Attach router to app
 app.use(router)
