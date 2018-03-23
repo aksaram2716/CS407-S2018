2cf0f39f5cd7c72800768057a4d7cb958f891ad1
Akshit Gudoor <agudoor@purdue.edu>
2018-03-22 21:43:44 -0700

    Delete buggy 'Front End' added by previous teammates

--- Frontend/src/components/profile/picControl.js
+++
@@ -1,8 +0,0 @@
-app.controller("picControl", function($scope) {
-    $scope.image = {
-        path:"https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/import/2013/images/2010/02/duckyduck.jpg?itok=a40QGDbT",
-    }
-    $scope.accountName = "theBestMallard92";
-    $scope.email = "duckingcrazy@hotmail.com"
-    $scope.realName= "Dale Flots";
-});
